5f4ade71527a7ac13c3709de0c04ae4cb38940da
sarindy <sarindyo@yahoo.com>
2016-02-03 15:08:33 +0700

    First Commit

---
+++ SQLServerConnectionModel/DBMgr.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data.SqlClient
+Public Class DBMgr
+    Private strConn As String
+
+
+    Private cnn As New SqlConnection("Server=localhost;Database=OSD001;User Id=dy;Password=qwe;")
+    Public Property GetConn() As SqlConnection
+        Get
+            Return cnn
+           
+
+
+
+        End Get
+        Set(ByVal value As SqlConnection)
+            cnn = value
+        End Set
+    End Property
+
+End Class
